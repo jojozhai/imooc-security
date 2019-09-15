0d907d6cdc7acd71a2b4d83da2297f0c40183e9b
zhailiang <zhailiang@haoduocheyou.com>
2019-09-15 15:02:47 +0800

    update

--- is-server-auth/src/main/java/com/imooc/security/server/auth/OAuth2AuthServerConfig.java
+++ is-server-auth/src/main/java/com/imooc/security/server/auth/OAuth2AuthServerConfig.java
@@ -8,6 +8,7 @@ import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
@@ -16,7 +17,9 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
 import org.springframework.session.jdbc.config.annotation.web.http.EnableJdbcHttpSession;
 
 /**
@@ -39,14 +42,24 @@ public class OAuth2AuthServerConfig extends AuthorizationServerConfigurerAdapter
 	
 	@Bean
 	public TokenStore tokenStore() {
-		return new JdbcTokenStore(dataSource);
+//		return new JdbcTokenStore(dataSource);
+		return new JwtTokenStore(jwtTokenEnhancer());
 	}
 	
+	private JwtAccessTokenConverter jwtTokenEnhancer() {
+		JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+//		converter.setSigningKey("123456");
+		KeyStoreKeyFactory keyStoreKeyFactory = new KeyStoreKeyFactory(new ClassPathResource("jojo.key"), "123456".toCharArray());
+		converter.setKeyPair(keyStoreKeyFactory.getKeyPair("jojo"));
+		return converter;
+	}
+
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
 		endpoints
 			.userDetailsService(userDetailsService)
 			.tokenStore(tokenStore())
+			.tokenEnhancer(jwtTokenEnhancer())
 			.authenticationManager(authenticationManager);
 	}
 	
@@ -57,7 +70,8 @@ public class OAuth2AuthServerConfig extends AuthorizationServerConfigurerAdapter
 	
 	@Override
 	public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
-		security.checkTokenAccess("isAuthenticated()");
+		security.tokenKeyAccess("isAuthenticated()")
+			    .checkTokenAccess("isAuthenticated()");
 	}
 	
 
