ca70e64f1cb085228287ef2b1f24809dfaddc800
zhailiang <zhailiang@haoduocheyou.com>
2019-10-06 19:03:19 +0800

    update

--- is-order-api/src/main/java/com/imooc/security/config/SentinelConfig.java
+++ is-order-api/src/main/java/com/imooc/security/config/SentinelConfig.java
@@ -3,33 +3,40 @@
  */
 package com.imooc.security.config;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.context.ApplicationListener;
-import org.springframework.context.event.ContextRefreshedEvent;
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import com.alibaba.csp.sentinel.slots.block.RuleConstant;
+import com.alibaba.csp.sentinel.datasource.ReadableDataSource;
+import com.alibaba.csp.sentinel.datasource.zookeeper.ZookeeperDataSource;
 import com.alibaba.csp.sentinel.slots.block.flow.FlowRule;
 import com.alibaba.csp.sentinel.slots.block.flow.FlowRuleManager;
+import com.alibaba.fastjson.JSON;
 
 /**
  * @author jojo
  *
  */
 @Component
-public class SentinelConfig implements ApplicationListener<ContextRefreshedEvent> {
+public class SentinelConfig {
 
-	@Override
-	public void onApplicationEvent(ContextRefreshedEvent event) {
-		FlowRule rule = new FlowRule();
-		rule.setResource("createOrder");
-		rule.setGrade(RuleConstant.FLOW_GRADE_QPS);
-		rule.setCount(1);
-		List<FlowRule> rules = new ArrayList<FlowRule>();
-		rules.add(rule);
-		FlowRuleManager.loadRules(rules);
+	@Value("${sentinel.zookeeper.address}")
+	private String zkServer;
+	
+	@Value("${sentinel.zookeeper.path}")
+	private String zkPath;
+	
+	@Value("${spring.application.name}")
+	private String appName;
+	
+	@PostConstruct
+	public void loadRules() {
+		ReadableDataSource<String, List<FlowRule>> flowRuleDataSource = new ZookeeperDataSource<>(zkServer, zkPath+"/"+appName, 
+			source -> JSON.parseArray(source, FlowRule.class));
+		FlowRuleManager.register2Property(flowRuleDataSource.getProperty());
 	}
-
+	
 }
